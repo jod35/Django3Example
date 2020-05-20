d94043f55fb5e75322bfee075de504eb94fdd85a
jona <jona@jona.com>
2020-05-20 06:19:33 +0300

    created urls for the views and then created a url for all the patterns in the main project

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#this view displays a list of posts
+def post_list(request):
+    posts=Post.objects.all()
+    return render('blog/post_list.html',{'posts':posts})
+
+
+#this view displays a detail of a single post
+def post_detail(request,year,month,day,post):
+    post=get_object_or_404(Post,
+    slug=post,
+    status='published',
+    publish_year=year,
+    publish_month=month,
+    publish_day=day
+    )
+
+    return render(request,'blog/post_detail.html',{'post':post})

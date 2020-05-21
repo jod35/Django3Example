125ebbf9ee2a637f3c584719f9c3d62d3e013523
jona <jona@jona.com>
2020-05-22 02:52:25 +0300

    successfully implemented the fulltext search

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from .models import Post,Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm,CommentForm
+from .forms import EmailPostForm,CommentForm,SearchForm
 from taggit.models import Tag
 from django.db.models import Count
 
@@ -111,3 +111,32 @@ def post_share(request,post_id):
     }
 
     return render(request,'blog/post_share.html',context)
+
+
+def post_search(request):
+    form=SearchForm()
+    query=None
+
+    results=[]
+
+    if query in request.GET:
+        form=SearchForm(request.GET)
+        if form.is_valid():
+
+            query=form.cleaned_data['query']
+
+            results=Post.objects.filter(status='published',search='query').annotate(
+                search=SearchVector('title','body')
+            )
+
+
+    context={
+        'form':form,
+        'query':query,
+        'results':results
+    }
+
+
+    return render(request,'blog/post-search.html',context)
+
+
